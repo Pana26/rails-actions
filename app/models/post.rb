0800f9c4e6be61879caf781b7060504ab3e1e249
Pana26 <57517345+Pana26@users.noreply.github.com>
2019-12-28 20:23:19 +0900

    show validations message

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
    has_rich_text :content
 
-  validates :title, length: { maximum: 32 }, presence: true
+  validates :title, length: { maximum: 16 }, presence: true
 
 end
